48272b756dbda6a449b165e4b810a024644091d0
Francesco <francesco.invidia.98@gmail.com>
2020-01-15 11:10:34 +0100

    try

--- sketch.js
+++ sketch.js
@@ -72,12 +72,13 @@ function draw() {
 
 
 
-
+push();
   fill('white')
   textSize(20)
   textAlign(CENTER,CENTER)
   text('Uncover your image', width/2-200,20,300,400)
   text('Press UP and DOWN arrows, and click to give some color', width/2-200,height-70,1000,400)
+  pop();
 
 }
 
